5612af79510c881d7a63deed267ca23c5c4402ff
JBamskyBam <168912311+JBamskyBam@users.noreply.github.com>
2024-08-09 17:43:01 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,18 @@
+# module_3_4
+
+def single_root_words(root_word, *other_words):
+    root_word = root_word.lower()  # Приводим к нижнему регистру
+    same_words = []
+
+    for word in other_words:
+        if root_word in word.lower() or word.lower() in root_word:
+            same_words.append(word)
+
+    return same_words
+
+# Примеры использования функции
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+print(result2)
